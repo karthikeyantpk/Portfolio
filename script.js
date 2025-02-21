c829d59d18ea429ca2edf484bc8bcec19a592a0f
karthikeyantpk <karthikeyantpk007@gmail.com>
2025-02-21 15:34:44 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,5 @@
+document.addEventListener("DOMContentLoaded", () => {
+    document.querySelectorAll(".slide-up").forEach((el) => {
+        el.style.animation = "slideUp 1.5s ease-in-out";
+    });
+});
